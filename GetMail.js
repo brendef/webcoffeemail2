88410bf4a59763f1894f64eb36ae365ce06d0939
brendan <brendefaria@brendans-MBP.Home>
2019-01-25 00:36:19 +0200

    third commit

--- GetMail.js
+++ GetMail.js
@@ -9,16 +9,15 @@ const options = {
 
 const card = (from, header, body, sentiment) => {
   return `
-    <details>
-      <summary>
-        <span class="from">${from}</span>
-        <br>
-        ${header}
-      </summary>
-
-      <p class="body">${body}</p>
-      <p class="sentiment">${sentiment}</p>
-    </details>
+      <details>
+        <summary>
+          <span class="from">${from}</span>
+          <br>
+          ${header}
+        </summary>
+        <p class="body">${body}</p>
+        <p class="sentiment">polarity: ${sentiment}</p>
+      </details>
     `
 }
 
@@ -31,10 +30,17 @@ const processMail = (message) => {
   let emailstring = message
   emailjson = JSON.parse(emailstring)
 
-  document.getElementById('mail').innerHTML += card(
+  theParent = document.getElementById("mail");
+  theKid = document.createElement("span");
+  theKid.innerHTML = card(
     emailjson.email.from,
     emailjson.email.header,
     emailjson.email.body,
     emailjson.email.sentiment
-  )
+  );
+
+  theParent.appendChild(theKid);
+  theParent.insertBefore(theKid, theParent.firstChild);
+
+
 }
